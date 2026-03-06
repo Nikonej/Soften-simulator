80a9151be89fd0b90a37a5bfdad428fd1df66fb8
Nikonej <Nikolai@guss.dk>
2026-03-06 15:26:49 +0100

    selector

--- SoftenSimulator/GUI.js
+++ SoftenSimulator/GUI.js
@@ -1,5 +1,4 @@
 xselscroll = 0;
-yselscroll = 0;
 function GUI(){
     rect(0 - xscroll, 500 - yscroll, 800, 200)
     image(husimg, 10 - xscroll + xselscroll, 550 - yscroll, 60, 40)
@@ -9,23 +8,27 @@ function GUI(){
 function mouseClicked() {
     let x = floor((mouseX-xscroll)/zoom);
     let y = floor((mouseY-yscroll)/zoom);
-    /*if (mouseY>400){
-        if (mouseX<10 && mouseX>)
-    }*/
-    let selected = Hus;
-    let selectedarray = buildings.huse;
-    if (grid[x][y] == false){
-        if (Room(x,y,selected)){
-            selectedarray.push(new selected(x,y,selected));
+    if (mouseY>500){
+        if (mouseX - xselscroll >= 10 && mouseX - xselscroll <= 70){
+            selected = Hus;
+            selectedarray = buildings.huse;
+        } else if (mouseX - xselscroll >= 90 && mouseX - xselscroll <= 150){
+
+        }
+    } else {
+        if (grid[x][y] == false){
+            if (Room(x,y,selected)){
+                selectedarray.push(new selected(x, y, selected));
+            }
         }
     }
 }
 
 function mouseWheel(event) {
-    if (mouseY<500){
+    if (mouseY < 500){
         xscroll -= event.deltaX;
         yscroll -= event.deltaY;
-    } else if (xselscroll >= 0){
+    } else if (xselscroll - event.deltaX <= 0){
         xselscroll -= event.deltaX;
     }
 
