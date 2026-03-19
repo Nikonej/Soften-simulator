af4cf5c6da56d03f144b5b45afd12d5eda9651be
Nikonej <Nikolai@guss.dk>
2026-03-19 10:26:20 +0100

    dont press the white button

--- SoftenSimulator/GUI.js
+++ SoftenSimulator/GUI.js
@@ -1,4 +1,7 @@
 xselscroll = 0;
+showpriorities = false;
+swap1 = 0;
+swap2 = 0;
 function GUI(){
     z = z0/zoom
     rect((0 - xscroll)*z, (500 - yscroll)*z, 800*z, 200*z)
@@ -12,6 +15,46 @@ function GUI(){
     image(kontor1img, (90 - xscroll + xselscroll)*z, (550 - yscroll)*z, 60 * z, 40 * z)
     image(hotel1img, (170 - xscroll + xselscroll)*z, (550 - yscroll)*z, 60 * z, 40 * z)
     image(hospitalimg, (250 - xscroll + xselscroll)*z, (550 - yscroll)*z, 60 * z, 40 * z)
+    fill(255)
+    rect((750 - xscroll + xselscroll)*z, (500 - yscroll)*z, 50 * z, 20 * z)
+    if (showpriorities){
+        rect(100 -xscroll,0 - yscroll,600,700)
+        let arbejder = [];
+        for (let i = 0; i < prioriteretArbejde.length; i++){
+            if (prioriteretArbejde[i].length>0){
+                arbejder.push(prioriteretArbejde[i]);
+            }
+        }
+        for (let i = 0; i < arbejder.length; i++){
+            switch (arbejder[i][0].constructor.name){
+                case "Kontor":
+                    image(kontor1img, 370 - xscroll, i*40 + 10 - yscroll, 60*z, 40*z);
+                break;
+                case "Hospital":
+                    image(hospitalimg, 370 - xscroll, i*40 + 10 - yscroll, 60*z, 40*z);
+                break;
+            }
+        }
+        if (mouseIsPressed){
+            if (!swap1){
+                swap1 = arbejder[floor((mouseY-10)/40)]
+                print(swap1);
+            } else {
+                swap2 = arbejder[floor((mouseY-10)/40)]
+                for (let i = 0; i < prioriteretArbejde; i++){
+                    print(2)
+                    print(prioriteretArbejde[i])
+                    if(str(prioriteretArbejde[i]) == swap1){
+                        print("swapper")
+                        prioriteretArbejde[i] = swap2;
+                    } else if (prioriteretArbejde[i] == swap2){
+                        print("swapper")
+                        prioriteretArbejde[i] = swap1;
+                    }
+                }
+            }
+        }
+    }
 
     switch (selected){
         case Hus:
@@ -39,15 +82,14 @@ function GUI(){
             pop;
         break;
     }
-
     if (mouseY>500){
-        if (mouseX - xselscroll >= 10 && mouseX - xselscroll <= 70){
+        if (mouseX - xselscroll >= 10 && mouseX - xselscroll <= 70 && mouseY >= 540){
             showStats(Hus);
-        } else if (mouseX - xselscroll >= 90 && mouseX - xselscroll <= 150){
+        } else if (mouseX - xselscroll >= 90 && mouseX - xselscroll <= 150 && mouseY >= 540){
             showStats(Kontor);
-        } else if (mouseX - xselscroll >= 170 && mouseX - xselscroll <= 230){
+        } else if (mouseX - xselscroll >= 170 && mouseX - xselscroll <= 230 && mouseY >= 540){
             showStats(Hotel);
-        } else if (mouseX - xselscroll >= 250 && mouseX - xselscroll <= 310){
+        } else if (mouseX - xselscroll >= 250 && mouseX - xselscroll <= 310 && mouseY >= 540){
             showStats(Hospital);
         }
     }
@@ -64,21 +106,23 @@ function mouseClicked() {
     let x = floor((mouseX-xscroll)/zoom);
     let y = floor((mouseY-yscroll)/zoom);
     if (mouseY>500){
-        if (mouseX - xselscroll >= 10 && mouseX - xselscroll <= 70){
+        if (mouseX - xselscroll >= 10 && mouseX - xselscroll <= 70 && mouseY >= 540){
             selected = Hus;
             selectedarray = buildings.huse;
-        } else if (mouseX - xselscroll >= 90 && mouseX - xselscroll <= 150){
+        } else if (mouseX - xselscroll >= 90 && mouseX - xselscroll <= 150 && mouseY >= 540){
             selected = Kontor;
             selectedarray = buildings.kontorer;
-        } else if (mouseX - xselscroll >= 170 && mouseX - xselscroll <= 230){
+        } else if (mouseX - xselscroll >= 170 && mouseX - xselscroll <= 230 && mouseY >= 540){
             selected = Hotel;
             selectedarray = buildings.hoteller;
-        } else if (mouseX - xselscroll >= 250 && mouseX - xselscroll <= 310){
+        } else if (mouseX - xselscroll >= 250 && mouseX - xselscroll <= 310 && mouseY >= 540){
             selected = Hospital;
             selectedarray = buildings.hospitaler;
+        } else if (mouseX - xselscroll >= 750 && mouseY < 520){
+            showpriorities = true;
         }
     } else {
-        if (grid[x][y] == false){
+        if (grid[x][y] == false && !showpriorities){
             if (Room(x,y,selected) && selected.price <= money){
               //  selectedarray.push(new selected(x, y, selected));
                 buildings.constructionsite.push(new Constructionsite(x, y, selected, selectedarray));
