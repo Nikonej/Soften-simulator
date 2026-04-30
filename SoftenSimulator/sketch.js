ff14d23b49fe6700f11ad22f31d9537a13352daf
Nikonej <Nikolai@guss.dk>
2026-04-30 10:11:17 +0200

    chrome update

--- SoftenSimulator/sketch.js
+++ SoftenSimulator/sketch.js
@@ -96,7 +96,7 @@ async function setup() {
         population.push(new Human(random(15,45), buildings.huse[0]));
     }
     speedSlider = createSlider(1,3,2,0);
-    speedSlider.position(745,568);
+    speedSlider.position(725,550);
 }
 
 function draw() {
